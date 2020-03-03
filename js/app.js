81f11341964dd43ac37c8f79595cbbd445430867
majdalkilany <majdkilany922@gmail.com>
2020-03-03 21:09:59 +0200

    mmodified the three defrent image

--- js/app.js
+++ js/app.js
@@ -14,15 +14,15 @@ var thiredImageRandom;
 
 var itemsArr = [];//an array to store all  objects
 var totalClicks = 0;
-var testArr = []; 
-var storeArr= [];
+var testArr = [];
+var storeArr = [];
 
 function Items(name) {
     this.name = name.split('.')[0]
     this.UrlImages = `images/${this.name}.jpg`;
     itemsArr.push(this);
     this.viwe = 0;
-    this.clicsks =0;
+    this.clicsks = 0;
 }
 function pickRandomImages() {
     firstImageRandom = itemsArr[randomNumber(0, itemsArr.length - 1)];
@@ -39,110 +39,90 @@ function pickRandomImages() {
     thieredItemImage.setAttribute('src', thiredImageRandom.UrlImages);
     thieredItemImage.setAttribute('alt', thiredImageRandom.name);
 
-    while(firstItemImage.alt === secundItemImage.alt || firstItemImage.alt === thieredItemImage.alt || secundItemImage.alt === thieredItemImage.alt || testArr.includes(firstImageRandom)|| testArr.includes(secundImageRandom)||testArr.includes(thiredImageRandom)) {
+    while (firstItemImage.alt === secundItemImage.alt || firstItemImage.alt === thieredItemImage.alt || secundItemImage.alt === thieredItemImage.alt || testArr.includes(firstImageRandom) || testArr.includes(secundImageRandom) || testArr.includes(thiredImageRandom)) {
         console.log('majd');
         firstImageRandom = itemsArr[randomNumber(0, itemsArr.length - 1)];
         secundImageRandom = itemsArr[randomNumber(0, itemsArr.length - 1)];
         thiredImageRandom = itemsArr[randomNumber(0, itemsArr.length - 1)];
-        
-    firstItemImage.setAttribute('src', firstImageRandom.UrlImages);
-    firstItemImage.setAttribute('alt', firstImageRandom.name);
-firstImageRandom.viwe++
-    secundItemImage.setAttribute('src', secundImageRandom.UrlImages);
-    secundItemImage.setAttribute('alt', secundImageRandom.name);
-secundImageRandom.viwe++
-    thieredItemImage.setAttribute('src', thiredImageRandom.UrlImages);
-    thieredItemImage.setAttribute('alt', thiredImageRandom.name);
-         thiredImageRandom.viwe++
-        }
 
-    // firstItemImage.setAttribute('src', firstImageRandom.UrlImages);
-    // firstItemImage.setAttribute('alt', firstImageRandom.name);
-    // secundItemImage.setAttribute('src', secundImageRandom.UrlImages);
-    // secundItemImage.setAttribute('alt', secundImageRandom.name);
-    // // 
-    // thieredItemImage.setAttribute('src', thiredImageRandom.UrlImages);
-    testArr =[];
+        firstItemImage.setAttribute('src', firstImageRandom.UrlImages);
+        firstItemImage.setAttribute('alt', firstImageRandom.name);
+        firstImageRandom.viwe++
+        secundItemImage.setAttribute('src', secundImageRandom.UrlImages);
+        secundItemImage.setAttribute('alt', secundImageRandom.name);
+        secundImageRandom.viwe++
+        thieredItemImage.setAttribute('src', thiredImageRandom.UrlImages);
+        thieredItemImage.setAttribute('alt', thiredImageRandom.name);
+        thiredImageRandom.viwe++
+    }
+
+    testArr = [];
     testArr.push(firstImageRandom);
     testArr.push(secundImageRandom);
     testArr.push(thiredImageRandom);
-     // thieredItemImage.setAttribute('alt', thiredImageRandom.name);
 }
 
 for (var i = 0; i < itemsEmg.length; i++) {
     new Items(itemsEmg[i]);
 }
 
-// for( var j =0 ; j<itemsArr.length; j++ ){
-    // }
-//     }
-// //     if ( firstItemImage.alt=== itemsArr[j].name){
-// //         itemsArr[j].viwe++
-// //     }
-// //     if ( secundItemImage.alt=== itemsArr[j].name){
-// //         itemsArr[j].viwe++
-// //     }
-// //     // for( var j =0 ; j<itemsArr.length; j++ )
-// //     if ( thieredItemImage.alt=== itemsArr[j].name){
-// //         itemsArr[j].viwe++
-    groupItemSection.addEventListener('click', clicksImagesEvint)
-                
-    //helper functions
-    function randomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+groupItemSection.addEventListener('click', clicksImagesEvint)
+
+//helper functions
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 pickRandomImages();
-function clicksImagesEvint(event){ 
-     if (event.target.id === 'first-item-img' || event.target.id === 'secund-item-img' || event.target.id === 'thered-item-img') {
-                        // itemsArr.viwe++
+function clicksImagesEvint(event) {
+    if (event.target.id === 'first-item-img' || event.target.id === 'secund-item-img' || event.target.id === 'thered-item-img') {
+        // itemsArr.viwe++
 
-    pickRandomImages();
-    totalClicks++;
-    
-      }
-      if (event.target.id === 'first-item-img'){
+        pickRandomImages();
+        totalClicks++;
+
+    }
+    if (event.target.id === 'first-item-img') {
         firstImageRandom.clicsks++
 
         // firstImageRandom.viwe++
 
-      }
-      if (event.target.id === 'secund-item-img'){
+    }
+    if (event.target.id === 'secund-item-img') {
         firstImageRandom.clicsks++
         // firstImageRandom.viwe++
 
-      }
-      if (event.target.id === 'thered-item-img'){
+    }
+    if (event.target.id === 'thered-item-img') {
         firstImageRandom.clicsks++
         // firstImageRandom.viwe++
 
-      }
-      
-     
-      if (totalClicks === 25) {
+    }
+
+
+    if (totalClicks === 25) {
         groupItemSection.removeEventListener('click', clicksImagesEvint);
-       listItem()
-       renderChartResults();
-      }
+        listItem()
+        renderChartResults();
+    }
 
 }
-for( var j =0 ; j<itemsArr.length; j++ ){
-    if ( firstItemImage.alt=== itemsArr[j].name){
+for (var j = 0; j < itemsArr.length; j++) {
+    if (firstItemImage.alt === itemsArr[j].name) {
         itemsArr[j].viwe++
     }
-    if ( secundItemImage.alt=== itemsArr[j].name){
+    if (secundItemImage.alt === itemsArr[j].name) {
         itemsArr[j].viwe++
     }
-    // for( var j =0 ; j<itemsArr.length; j++ )
-    if ( thieredItemImage.alt=== itemsArr[j].name){
+    if (thieredItemImage.alt === itemsArr[j].name) {
         itemsArr[j].viwe++
     }
-    }
-    groupItemSection.addEventListener('click', clicksImagesEvint)
-                
-    //helper functions
-    function randomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+}
+groupItemSection.addEventListener('click', clicksImagesEvint)
+
+//helper functions
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 // render 
@@ -158,65 +138,64 @@ function listItem() {
 }
 var itemsName = [];
 var itemsclick = [];
-var ItemsViwe =[];
+var ItemsViwe = [];
 
-function renderChartResults(){
-  for ( var i= 0 ; i < itemsArr.length ; i++){
-    var nameOfArr = itemsArr[i].name
-    var likeItems = itemsArr[i].clicsks
-    var viewChart = itemsArr[i].viwe
+function renderChartResults() {
+    for (var i = 0; i < itemsArr.length; i++) {
+        var nameOfArr = itemsArr[i].name
+        var likeItems = itemsArr[i].clicsks
+        var viewChart = itemsArr[i].viwe
 
-     itemsName.push(nameOfArr);
-     itemsclick.push(likeItems);
-     ItemsViwe.push(viewChart);
+        itemsName.push(nameOfArr);
+        itemsclick.push(likeItems);
+        ItemsViwe.push(viewChart);
 
-  }
+    }
 
-  var ctx = document.getElementById('myItems').getContext('2d');
-  var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: itemsName,
-          datasets: [{
-              label: '# clicks',
-              data: itemsclick,
-              // data:ItemsViwe, 
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            },{
-              label: '# view',
+    var ctx = document.getElementById('myItems').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: itemsName,
+            datasets: [{
+                label: '# clicks',
+                data: itemsclick,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# view',
 
-              labels : itemsName, 
-               data:ItemsViwe, 
-
+                labels: itemsName,
+                data: ItemsViwe,
 
 
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
-      }
-  });
-  
+
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
 }
